5d8a2ec43562b0a9001d8ede0e0c9d286f379653
bhavaniravi <bhava0895@gmail.com>
2021-12-27 01:15:51 +0530

    add tekton pipelines

--- ci-cd/ambassador/ambassdor.sh
+++ ci-cd/ambassador/ambassdor.sh
@@ -1,2 +1,6 @@
 kubectl apply -f https://www.getambassador.io/yaml/aes-crds.yaml && kubectl wait --for condition=established --timeout=90s crd -lproduct=aes && kubectl apply -f https://www.getambassador.io/yaml/aes.yaml && kubectl -n ambassador wait --for condition=available --timeout=90s deploy -lproduct=aes
 AMBASSADOR_IP=$(kubectl get -n ambassador service ambassador -o "go-template={{range .status.loadBalancer.ingress}}{{or .ip .hostname}}{{end}}")
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.0.0/cert-manager.crds.yaml
+helm repo add jetstack https://charts.jetstack.io && helm repo update
+kubectl create ns cert-manager
+helm install cert-manager --namespace cert-manager jetstack/cert-manager
---
+++ ci-cd/ambassador/certs.sh
@@ -0,0 +1,4 @@
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.0.0/cert-manager.crds.yaml
+helm repo add jetstack https://charts.jetstack.io && helm repo update
+kubectl create ns cert-manager
+helm install cert-manager --namespace cert-manager jetstack/cert-manager
